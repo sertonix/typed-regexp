2512169f8f2b314e71f5ba8e6ac1d056d81bae6e
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-20 13:17:08 +0100

    new test style

--- tests/tests.ts
+++ tests/tests.ts
@@ -5,18 +5,36 @@ import type {
 } from "..";
 
 // --- Tools for testings ---
-type E<A,B> = A extends B ? B extends A ? true : false : false;
-declare const t: <T extends true>(t: T) => T;
+type TrueTrue<T> = {[t in T as t extends false ? "" : never]: true} extends {[t:string]: never} ? true : false;
+type MatchType<A,B> = TrueTrue<A extends B ? B extends A ? true : false : false>;
+declare const e: <A,B>( this: MatchType<A,B> extends true ? void : {expected:A,got:B} ) => void;
+declare const t: <A>( a: A ) => <B>( this: MatchType<A,B> extends true ? void : {expected:B,got:A} ) => void;
 
 // --- Ensure test system works ---
-t<true>;
+e<1,1>();
 // @ts-expect-error
-t<false>;
-t<E<1,1>>;
+e<1,2>();
 // @ts-expect-error
-t<E<1,1|2>>;
+e<1,1|2>();
+// @ts-expect-error
+e<1,2>();
+t(2 as const)<2>();
+// @ts-expect-error
+t(2 as const)<1>();
+// @ts-expect-error
+t(2 as const)<1|2>();
 
 // --- Tests ---
 
 const match = "".match(/ / as TypedRegExp<["a"],any,"">);
-if (match) t<E<"a",typeof match[1]>>;
+
+t(match)<TypedRegExpMatchArray<["a"],any>|null>();
+
+if (match) {
+  t(match)<TypedRegExpMatchArray<["a"],any>>();
+  t(match[0])<string>();
+  t(match[1])<"a">();
+  t(match[2])<undefined>();
+} else {
+  t(match)<null>();
+}
