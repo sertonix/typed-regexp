2647d13c6248b46dce8620751dccfb2a3c357cfc
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-20 17:23:45 +0100

    improved type testing system

--- tests/tests.ts
+++ tests/tests.ts
@@ -5,8 +5,7 @@ import type {
 } from "..";
 
 // --- Tools for testings ---
-type TrueTrue<T> = {[t in T as t extends false ? "" : never]: true} extends {[t:string]: never} ? true : false;
-type MatchType<A,B> = TrueTrue<A extends B ? B extends A ? true : false : false>;
+type MatchType<A,B> = [A] extends [B] ? [B] extends [A] ? true : false : false;
 declare const e: <A,B>( this: MatchType<A,B> extends true ? void : {expected:A,got:B} ) => void;
 declare const t: <A>( a: A ) => <B>( this: MatchType<A,B> extends true ? void : {expected:B,got:A} ) => void;
 
