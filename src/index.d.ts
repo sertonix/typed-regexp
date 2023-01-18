3fca6bf91db03097e8b21b4bc1c10e51a20fb2c2
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-18 08:15:58 +0100

    prefix local type names with T

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,37 +7,37 @@ import type {
   SortRegExpFlags,
 } from "./utils";
 
-export type TypedRegExpMatchArray<Groups extends string[] = string[], NamedGroups extends { [key: string]: string } = { [key: string]: string }, InputString extends string = string> = {
+export type TypedRegExpMatchArray<TGroups extends string[] = string[], TNamedGroups extends { [key: string]: string } = { [key: string]: string }, TInputString extends string = string> = {
     index?: number;
-    input?: InputString;
-    groups: OneNotOptional<NamedGroups>;
-} & ( [string,...Groups] & IntersectedArray<string,TypedRegExpMatchArray<Groups,NamedGroups,InputString>> );
+    input?: TInputString;
+    groups: OneNotOptional<TNamedGroups>;
+} & ( [string,...TGroups] & IntersectedArray<string,TypedRegExpMatchArray<TGroups,TNamedGroups,TInputString>> );
 
-export type TypedRegExpExecArray<Groups extends string[] = string[], NamedGroups extends { [key: string]: string } = { [key: string]: string }, InputString extends string = string> = TypedRegExpMatchArray<Groups,NamedGroups,InputString> & {
+export type TypedRegExpExecArray<TGroups extends string[] = string[], TNamedGroups extends { [key: string]: string } = { [key: string]: string }, TInputString extends string = string> = TypedRegExpMatchArray<TGroups,TNamedGroups,TInputString> & {
     index: number;
-    input: InputString;
+    input: TInputString;
 };
 
 export interface TypedRegExp<
-    Groups extends string[] = string[],
-    NamedGroups extends { [key: string]: string } = { [key: string]: string },
-    FlagCombo extends RegExpFlagCombos = RegExpFlagCombos,
+    TGroups extends string[] = string[],
+    TNamedGroups extends { [key: string]: string } = { [key: string]: string },
+    TFlagCombo extends RegExpFlagCombos = RegExpFlagCombos,
 > extends RegExp {
     // IDEA only override if existing in the RegExp interface to support more versions
-    exec<InputString extends string, Matches extends boolean = boolean>(string: InputString): If<Matches,TypedRegExpExecArray<Groups,NamedGroups,InputString>,null>;
-    readonly dotAll: HasFlag<FlagCombo,"d">;
-    readonly global: HasFlag<FlagCombo,"g">;
-    readonly ignoreCase: HasFlag<FlagCombo,"i">;
-    readonly multiline: HasFlag<FlagCombo,"m">;
-    readonly unicode: HasFlag<FlagCombo,"u">;
-    readonly sticky: HasFlag<FlagCombo,"y">;
-    readonly flags: SortRegExpFlags<FlagCombo>;
+    exec<TInputString extends string, TMatches extends boolean = boolean>(string: TInputString): If<TMatches,TypedRegExpExecArray<TGroups,TNamedGroups,TInputString>,null>;
+    readonly dotAll: HasFlag<TFlagCombo,"d">;
+    readonly global: HasFlag<TFlagCombo,"g">;
+    readonly ignoreCase: HasFlag<TFlagCombo,"i">;
+    readonly multiline: HasFlag<TFlagCombo,"m">;
+    readonly unicode: HasFlag<TFlagCombo,"u">;
+    readonly sticky: HasFlag<TFlagCombo,"y">;
+    readonly flags: SortRegExpFlags<TFlagCombo>;
     // this method is restricted to return a sub type so that the type safety is kept
-    compile< NewGroups extends Groups = Groups, NewNamedGroups extends NamedGroups = NamedGroups, NewFlagCombo extends FlagCombo = FlagCombo >(pattern: string, flags?: NewFlagCombo): this & TypedRegExp<NewGroups,NewNamedGroups,NewFlagCombo>;
-    [Symbol.match]<InputString extends string, Matches extends boolean = boolean>(string: InputString): If<Matches,TypedRegExpMatchArray<Groups,NamedGroups,InputString>,null>;
+    compile< TNewGroups extends TGroups = TGroups, TNewNamedGroups extends TNamedGroups = TNamedGroups, NewFlagCombo extends TFlagCombo = TFlagCombo >(pattern: string, flags?: NewFlagCombo): this & TypedRegExp<TNewGroups,TNewNamedGroups,NewFlagCombo>;
+    [Symbol.match]<TInputString extends string, TMatches extends boolean = boolean>(string: TInputString): If<TMatches,TypedRegExpMatchArray<TGroups,TNamedGroups,TInputString>,null>;
     [Symbol.replace](string: string, replaceValue: string): string;
-    [Symbol.replace](string: string, replacer: (substring: string, ...args: [...Groups,number,string,...(keyof NamedGroups extends never ? [] : [NamedGroups])]) => string): string;
-    [Symbol.matchAll]<InputString extends string>(str: InputString): IterableIterator<TypedRegExpMatchArray<Groups,NamedGroups,InputString>>;
+    [Symbol.replace](string: string, replacer: (substring: string, ...args: [...TGroups,number,string,...(keyof TNamedGroups extends never ? [] : [TNamedGroups])]) => string): string;
+    [Symbol.matchAll]<TInputString extends string>(str: TInputString): IterableIterator<TypedRegExpMatchArray<TGroups,TNamedGroups,TInputString>>;
 }
 
 export declare const TypedRegExp: TypedRegExpConstructor;
@@ -45,14 +45,14 @@ export declare const TypedRegExp: TypedRegExpConstructor;
 type RegExpConstructor = typeof RegExp;
 interface TypedRegExpConstructor extends RegExpConstructor {
     new (): TypedRegExp<[],{},"">;
-    new <Groups extends string[], NamedGroups extends { [key: string]: string }>(pattern: string): TypedRegExp<Groups,NamedGroups,"">;
-    new <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: string, flags: FlagCombo): TypedRegExp<Groups,NamedGroups,FlagCombo>;
-    new <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<Groups,NamedGroups,FlagCombo>): TypedRegExp<Groups,NamedGroups,FlagCombo>;
-    new <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<Groups,NamedGroups>, flags: FlagCombo): TypedRegExp<Groups,NamedGroups,FlagCombo>;
-    <Groups extends string[], NamedGroups extends { [key: string]: string }>(pattern: string): TypedRegExp<Groups,NamedGroups,"">;
-    <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: string, flags: FlagCombo): TypedRegExp<Groups,NamedGroups,FlagCombo>;
-    <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<Groups,NamedGroups,FlagCombo>): TypedRegExp<Groups,NamedGroups,FlagCombo>;
-    <Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<Groups,NamedGroups>, flags: FlagCombo): TypedRegExp<Groups,NamedGroups,FlagCombo>;
+    new <TGroups extends string[], TNamedGroups extends { [key: string]: string }>(pattern: string): TypedRegExp<TGroups,TNamedGroups,"">;
+    new <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: string, flags: TFlagCombo): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
+    new <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
+    new <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<TGroups,TNamedGroups>, flags: TFlagCombo): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
+    <TGroups extends string[], TNamedGroups extends { [key: string]: string }>(pattern: string): TypedRegExp<TGroups,TNamedGroups,"">;
+    <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: string, flags: TFlagCombo): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
+    <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
+    <TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos>(pattern: TypedRegExp<TGroups,TNamedGroups>, flags: TFlagCombo): TypedRegExp<TGroups,TNamedGroups,TFlagCombo>;
     readonly prototype: TypedRegExp;
     readonly [Symbol.species]: TypedRegExpConstructor;
 }
@@ -61,18 +61,18 @@ interface TypedRegExpConstructor extends RegExpConstructor {
 export interface TypedRegExpString extends Omit<String,"match"|"matchAll"|"replace"|"replaceAll"> /* excludes the old properties to prevent overloads */ {
     // IDEA use return value from the Regexp[Symbol.match] etc. definitions
     // IDEA pass this as string to matches
-    match<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos = RegExpFlagCombos, Matches extends boolean = boolean>(regexp: string | TypedRegExp<Groups,NamedGroups,FlagCombo>): If<Matches,TypedRegExpMatchArray<Groups,NamedGroups>,null>;
-    matchAll<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(regexp: TypedRegExp<Groups,NamedGroups,FlagCombo>): IterableIterator<TypedRegExpMatchArray<Groups,NamedGroups>>;
-    replace<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos = RegExpFlagCombos>(searchValue: string | TypedRegExp<Groups,NamedGroups,FlagCombo>, replacer: (substring: string, ...args: [...Groups,number,string,...(keyof NamedGroups extends never ? [] : [NamedGroups])]) => string): string;
-    replaceAll<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(searchValue: string | TypedRegExp<Groups,NamedGroups,FlagCombo>, replacer: (substring: string, ...args: [...Groups,number,string,...(keyof NamedGroups extends never ? [] : [NamedGroups])]) => string): string;
+    match<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos = RegExpFlagCombos, TMatches extends boolean = boolean>(regexp: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): If<TMatches,TypedRegExpMatchArray<TGroups,TNamedGroups>,null>;
+    matchAll<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(regexp: TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): IterableIterator<TypedRegExpMatchArray<TGroups,TNamedGroups>>;
+    replace<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos = RegExpFlagCombos>(searchValue: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>, replacer: (substring: string, ...args: [...TGroups,number,string,...(keyof TNamedGroups extends never ? [] : [TNamedGroups])]) => string): string;
+    replaceAll<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(searchValue: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>, replacer: (substring: string, ...args: [...TGroups,number,string,...(keyof TNamedGroups extends never ? [] : [TNamedGroups])]) => string): string;
 }
 
 declare global {
     interface String {
         // IDEA add a toggle to apply the modifications to the String class directly
-        match<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos = RegExpFlagCombos, Matches extends boolean = boolean>(regexp: string | TypedRegExp<Groups,NamedGroups,FlagCombo>): If<Matches,TypedRegExpMatchArray<Groups,NamedGroups>,null>;
-        matchAll<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(regexp: TypedRegExp<Groups,NamedGroups,FlagCombo>): IterableIterator<TypedRegExpMatchArray<Groups,NamedGroups>>;
-        replace<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos = RegExpFlagCombos>(searchValue: string | TypedRegExp<Groups,NamedGroups,FlagCombo>, replacer: (substring: string, ...args: [...Groups,number,string,...(keyof NamedGroups extends never ? [] : [NamedGroups])]) => string): string;
-        replaceAll<Groups extends string[], NamedGroups extends { [key: string]: string }, FlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(searchValue: string | TypedRegExp<Groups,NamedGroups,FlagCombo>, replacer: (substring: string, ...args: [...Groups,number,string,...(keyof NamedGroups extends never ? [] : [NamedGroups])]) => string): string;
+        match<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos = RegExpFlagCombos, TMatches extends boolean = boolean>(regexp: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): If<TMatches,TypedRegExpMatchArray<TGroups,TNamedGroups>,null>;
+        matchAll<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(regexp: TypedRegExp<TGroups,TNamedGroups,TFlagCombo>): IterableIterator<TypedRegExpMatchArray<TGroups,TNamedGroups>>;
+        replace<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos = RegExpFlagCombos>(searchValue: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>, replacer: (substring: string, ...args: [...TGroups,number,string,...(keyof TNamedGroups extends never ? [] : [TNamedGroups])]) => string): string;
+        replaceAll<TGroups extends string[], TNamedGroups extends { [key: string]: string }, TFlagCombo extends RegExpFlagCombos<{g:true}> = RegExpFlagCombos<{g:true}>>(searchValue: string | TypedRegExp<TGroups,TNamedGroups,TFlagCombo>, replacer: (substring: string, ...args: [...TGroups,number,string,...(keyof TNamedGroups extends never ? [] : [TNamedGroups])]) => string): string;
     }
 }
